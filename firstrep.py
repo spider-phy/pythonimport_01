c461e81b3c4bf9146ef3dbb879ff1b2c8a3911bd
spider-phy <110330427+spider-phy@users.noreply.github.com>
2022-07-31 14:54:07 +0530

    Add files via upload

---
+++ firstrep.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+import os
+
+
+# In[3]:
+
+
+os.getcwd()
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+pd.read_csv("mine.tsv")
+
+
+# In[ ]:
+
+
+
+
